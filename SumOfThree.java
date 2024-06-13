b0162148430779f1450a91d4c9b0e0e3c1349f53
Steven Cognata <89099504+StevenCognata@users.noreply.github.com>
2024-06-12 21:24:25 -0400

    Create SumOfThree.java

---
+++ SumOfThree.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class SumOfThree{
+   public static boolean findSumOfThree(int[] nums, int target) {
+      Arrays.sort(nums);
+
+      for (int i = 0; i < nums.length - 2; i++){
+         int low  = i + 1;
+         int high = nums.length - 1;
+
+         while(low < high){
+            int sum = nums[i] + nums[low] + nums[high];
+            if(sum == target){
+               return true;
+            } else if(sum < target) {
+               low++;
+            } else {
+               high--;
+               }
+            }
+         }
+         return false;
+         }
+}
